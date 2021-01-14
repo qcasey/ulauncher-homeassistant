66e9a05950a2d2dc773a5cfb861598125d538871
Quinn Casey <qcasey1@gmail.com>
2021-01-14 11:58:32 -0800

    fix #4, rewrite for efficiency and clarity

--- main.py
+++ main.py
@@ -9,10 +9,29 @@ from ulauncher.api.shared.action.RenderResultListAction import RenderResultListA
 from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 
-ON_OFF_TYPES = ["light", "switch", "automation", "scene", "group", "input_boolean", "media_player", "climate", "camera"]
-ACTION_WORDS = ["on", "off", "open", "close"]
+# Entity class types, effected by ON/OFF
+ON_OFF_TYPES = [
+    "light",
+    "switch",
+    "automation",
+    "scene",
+    "group",
+    "input_boolean",
+    "media_player",
+    "climate",
+    "camera",
+]
+# Entity class types, effected by OPEN/CLOSE
+OPEN_CLOSE_TYPES = ["cover"]
 
-icon_files = {
+ACTION_TYPE_MAP = {
+    "on": ON_OFF_TYPES,
+    "off": ON_OFF_TYPES,
+    "open": OPEN_CLOSE_TYPES,
+    "close": OPEN_CLOSE_TYPES,
+}
+
+ICON_FILES = {
     "logo": "images/icon.png",
     "automation": "images/automation.png",
     "cover": "images/cover.png",
@@ -40,7 +59,7 @@ class KeywordQueryEventListener(EventListener):
             return RenderResultListAction(
                 [
                     ExtensionResultItem(
-                        icon=icon_files["logo"],
+                        icon=ICON_FILES["logo"],
                         name="Invalid Home Assistant URL",
                         on_enter=HideWindowAction(),
                     )
@@ -54,7 +73,7 @@ class KeywordQueryEventListener(EventListener):
             return RenderResultListAction(
                 [
                     ExtensionResultItem(
-                        icon=icon_files["logo"],
+                        icon=ICON_FILES["logo"],
                         name="Empty Home Assistant API Key",
                         on_enter=HideWindowAction(),
                     )
@@ -65,7 +84,7 @@ class KeywordQueryEventListener(EventListener):
             return RenderResultListAction(
                 [
                     ExtensionResultItem(
-                        icon=icon_files["logo"],
+                        icon=ICON_FILES["logo"],
                         name="No input",
                         on_enter=HideWindowAction(),
                     )
@@ -73,14 +92,14 @@ class KeywordQueryEventListener(EventListener):
             )
 
         action_word = query.split()[0].lower().strip()
-        is_action_word = action_word in ACTION_WORDS
-        entity_query_list = query.split()[1:] if is_action_word else query.split()
+        is_action_word = action_word in ACTION_TYPE_MAP.keys()
+        entity_query = query.split()[1:] if is_action_word else query.split()
 
-        if not entity_query_list:
+        if not entity_query:
             return RenderResultListAction(
                 [
                     ExtensionResultItem(
-                        icon=icon_files["logo"], name="", on_enter=HideWindowAction()
+                        icon=ICON_FILES["logo"], name="", on_enter=HideWindowAction()
                     )
                 ]
             )
@@ -97,7 +116,7 @@ class KeywordQueryEventListener(EventListener):
             return RenderResultListAction(
                 [
                     ExtensionResultItem(
-                        icon=icon_files["logo"],
+                        icon=ICON_FILES["logo"],
                         name=response.text,
                         on_enter=HideWindowAction(),
                     )
@@ -106,10 +125,6 @@ class KeywordQueryEventListener(EventListener):
 
         # Parse all entities and states
         for entity in json.loads(response.text):
-            # May want to make this configurable later
-            if len(items) > 6:
-                break
-
             # Not likely, but worth checking
             if "entity_id" not in entity or "attributes" not in entity:
                 continue
@@ -117,86 +132,77 @@ class KeywordQueryEventListener(EventListener):
             if "friendly_name" not in entity["attributes"]:
                 entity["attributes"]["friendly_name"] = entity["entity_id"]
 
+            entity_class = entity["entity_id"].split(".")[0]
+            entity_icon = (
+                ICON_FILES[entity_class]
+                if entity_class in ICON_FILES
+                else ICON_FILES["logo"]
+            )
+
+            # Validate action word against entity
+            if is_action_word:
+                # If action word doesn't apply to entity
+                if entity_class not in ACTION_TYPE_MAP[action_word]:
+                    continue
+
+                # If action word wouldn't affect entity
+                if entity["state"] == action_word or entity["state"] == "unavailable":
+                    continue
+
             # Don't add this item if the query doesn't appear in either friendly_name or id
             entity_appears_in_search = True
-            for entity_query_item in entity_query_list:
+            for query_word in entity_query:
                 if (
-                    entity_query_item
-                    not in entity["attributes"]["friendly_name"].lower()
-                    and entity_query_item not in entity["entity_id"]
+                    query_word not in entity["attributes"]["friendly_name"].lower()
+                    and query_word not in entity["entity_id"]
                 ):
                     entity_appears_in_search = False
 
             if not entity_appears_in_search:
                 continue
 
-            entity_class = entity["entity_id"].split(".")[0]
-            entity_icon = (
-                icon_files[entity_class]
-                if entity_class in icon_files
-                else icon_files["logo"]
-            )
-
             # If we require an action beyond just displaying state
             if is_action_word:
+                item_name = ""
+                endpoint = ""
 
                 # ON / OFF action
                 if action_word == "on" or action_word == "off":
-                    if entity_class in ON_OFF_TYPES:
-                        data = {
-                            "endpoint": "{}/api/services/homeassistant/turn_{}".format(
-                                hass_url, action_word
-                            ),
-                            "service_data": {"entity_id": entity["entity_id"]},
-                            "hass_key": hass_key,
-                            "headers": headers,
-                        }
-
-                        items.append(
-                            ExtensionResultItem(
-                                icon=entity_icon,
-                                name="Turn {} {}".format(
-                                    action_word,
-                                    entity["entity_id"],
-                                ),
-                                description="{} is currently {}".format(
-                                    entity["entity_id"],
-                                    entity["state"],
-                                ),
-                                on_enter=ExtensionCustomAction(
-                                    data, keep_app_open=False
-                                ),
-                            )
-                        )
+                    endpoint = "{}/api/services/homeassistant/turn_{}".format(
+                        hass_url, action_word
+                    )
+                    item_name = "Turn {} {}"
 
                 # OPEN / CLOSE action (for covers)
                 elif action_word == "open" or action_word == "close":
-                    if entity_class == "cover":
-                        data = {
-                            "endpoint": "{}/api/services/cover/{}_cover".format(
-                                hass_url, action_word
-                            ),
-                            "service_data": {"entity_id": entity["entity_id"]},
-                            "hass_key": hass_key,
-                            "headers": headers,
-                        }
+                    endpoint = "{}/api/services/cover/{}_cover".format(
+                        hass_url, action_word
+                    )
+                    item_name = "{} {}"
 
-                        items.append(
-                            ExtensionResultItem(
-                                icon=entity_icon,
-                                name="{} {}".format(
-                                    action_word,
-                                    entity["entity_id"],
-                                ),
-                                description="{} is currently {}".format(
-                                    entity["entity_id"],
-                                    entity["state"],
-                                ),
-                                on_enter=ExtensionCustomAction(
-                                    data, keep_app_open=False
-                                ),
-                            )
-                        )
+                # Append action item
+                items.append(
+                    ExtensionResultItem(
+                        icon=entity_icon,
+                        name=item_name.format(
+                            action_word,
+                            entity["entity_id"],
+                        ),
+                        description="{} is currently {}".format(
+                            entity["entity_id"],
+                            entity["state"],
+                        ),
+                        on_enter=ExtensionCustomAction(
+                            {
+                                "endpoint": endpoint,
+                                "service_data": {"entity_id": entity["entity_id"]},
+                                "hass_key": hass_key,
+                                "headers": headers,
+                            },
+                            keep_app_open=False,
+                        ),
+                    )
+                )
 
             # Otherwise, assume it's a state query
             else:
@@ -211,10 +217,15 @@ class KeywordQueryEventListener(EventListener):
                     )
                 )
 
+            # Check if we've reached the entity limit after adding
+            # May want to make this # configurable later
+            if len(items) > 6:
+                break
+
         if len(items) == 0:
             items.append(
                 ExtensionResultItem(
-                    icon=icon_files["logo"],
+                    icon=ICON_FILES["logo"],
                     name="Entity not found",
                     on_enter=HideWindowAction(),
                 )
